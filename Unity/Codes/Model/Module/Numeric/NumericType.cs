ad81bcd6223a2cc289ee96cc59c1ac8a62106aae
lywedo <lywedo@gmail.com>
2022-03-26 21:42:44 +0800

    第八集看一半

--- Unity/Codes/Model/Module/Numeric/NumericType.cs
+++ Unity/Codes/Model/Module/Numeric/NumericType.cs
@@ -12,8 +12,8 @@
 	    public const int SpeedFinalAdd = Speed * 10 + 4;
 	    public const int SpeedFinalPct = Speed * 10 + 5;
 
-	    public const int Hp = 1001;
-	    public const int HpBase = Hp * 10 + 1;
+	    // public const int Hp = 1001;
+	    // public const int HpBase = Hp * 10 + 1;
 
 	    public const int MaxHp = 1002;
 	    public const int MaxHpBase = MaxHp * 10 + 1;
@@ -29,11 +29,44 @@
 	    public const int AOIFinalAdd = AOI * 10 + 4;
 	    public const int AOIFinalPct = AOI * 10 + 5;
 
-	    public const int Level = 1004;
-	    public const int Gold = 1005;
-	    public const int Exp = 1006;
-	    public const int Position = 1007;
-	    public const int Height = 1008;
-	    public const int Weight = 1009;
+	    public const int DamageValue = 1011; //伤害
+	    public const int DamageValueBase = DamageValue * 10 + 1;
+	    public const int DamageValueAdd = DamageValue * 10 + 2;
+	    public const int DamageValuePct = DamageValue * 10 + 3;
+	    public const int DamageValueFinalAdd = DamageValue * 10 + 4;
+	    public const int DamageValueFinalPct = DamageValue * 10 + 5;
+
+	    public const int AdditionalDdamage = 1012; //伤害追加
+
+	    public const int Hp = 1013; //生命值
+	    public const int HpBase = Hp * 10 + 1;
+	    public const int HpAdd = Hp * 10 + 2;
+	    public const int HpPct = Hp * 10 + 3;
+	    public const int HpFinalAdd = Hp * 10 + 4;
+	    public const int HpFinalPct = Hp * 10 + 5;
+
+	    public const int Armor = 1015; //护甲
+	    public const int ArmorBase = Armor * 10 + 1;
+	    public const int ArmorAdd = Armor * 10 + 2;
+	    public const int ArmorPct = Armor * 10 + 3;
+	    public const int ArmorFinalAdd = Armor * 10 + 4;
+	    public const int ArmorFinalPct = Armor * 10 + 5;
+
+	    public const int AdditionalArmor = 1016; //护甲追加
+
+	    public const int Power = 3001; //力量
+	    public const int PhysicalStrength = 3002; //体力
+	    public const int Agile = 3003; //敏捷值
+	    public const int Spirit = 3004; //精神
+	    public const int AttributePoint = 3005; //属性点
+	    public const int CombatEffectiveness = 3006; //战力值
+
+
+	    public const int Level = 3007;
+	    public const int Gold = 3008;
+	    public const int Exp = 3009;
+	    public const int Position = 3010;
+	    public const int Height = 3011;
+	    public const int Weight = 3012;
     }
 }
